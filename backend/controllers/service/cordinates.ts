ed4affea89da6104759a163b7423a303a743434c
narsing <maheshwarinarsing@gmail.com>
2024-09-12 16:39:21 +0530

    Add files via upload

---
+++ backend/controllers/service/cordinates.ts
@@ -0,0 +1,66 @@
+const coordinates = [
+    {
+      "id": 1,
+      "coordinates": {
+        "latitude": 46.948734617700595,
+        "longitude": 7.442829220653488
+      }
+    },
+    {
+      "id": 2,
+      "coordinates": {
+        "latitude": 46.97263503155135,
+        "longitude": 7.461368648003226
+      }
+    },
+    {
+      "id": 3,
+      "coordinates": {
+        "latitude": 46.93631155998642,
+        "longitude": 7.402660461395724
+      }
+    },
+    {
+      "id": 4,
+      "coordinates": {
+        "latitude": 46.80656753886196,
+        "longitude": 7.162000832395455
+      }
+    },
+    {
+      "id": 5,
+      "coordinates": {
+        "latitude": 46.80098433595137,
+        "longitude": 7.144276573547655
+      }
+    },
+    {
+      "id": 6,
+      "coordinates": {
+        "latitude": 46.813967419320186,
+        "longitude": 7.1438474201367805
+      }
+    },
+    {
+      "id": 7,
+      "coordinates": {
+        "latitude": 47.37767418588649,
+        "longitude": 8.540683711380035
+      }
+    },
+    {
+      "id": 8,
+      "coordinates": {
+        "latitude": 47.359972679402865,
+        "longitude": 8.495189951774075
+      }
+    },
+    {
+      "id": 9,
+      "coordinates": {
+        "latitude": 47.37517046991252,
+        "longitude": 8.486802815757894
+      }
+    }
+  ]
+  export default coordinates;
